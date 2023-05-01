da9e1e2eb4c9e3b788d820ab171938c3d0497157
lyb4ooo <lyb4ooo@abv.bg>
2023-05-01 17:42:44 +0300

    new exercises

---
+++ 02-Basic CRUD - Lab.sql
@@ -0,0 +1,58 @@
+Problem 1 
+SELECT `id`, `first_name`, `last_name`, `job_title` from employees
+ORDER BY `id`;
+
+---------------------------------------------------------------
+Problem 2
+SELECT 
+    `id`,
+    CONCAT_WS(' ', `first_name`, `last_name`) AS `full_name`,
+    `job_title`,
+    `salary`
+FROM
+    `employees`
+WHERE
+    `salary` > 1000
+ORDER BY `id`;
+
+---------------------------------------------------------------
+Problem 3
+UPDATE `employees` 
+SET 
+    `salary` = `salary` + 100
+WHERE
+    `job_title` = 'Manager';
+SELECT 
+    `salary`
+FROM
+    `employees`;
+
+---------------------------------------------------------------
+Problem 4
+SELECT 
+    *
+FROM
+    `employees`
+ORDER BY `salary` DESC
+LIMIT 1;
+
+---------------------------------------------------------------
+Problem 5
+SELECT 
+    *
+FROM
+    `employees`
+WHERE
+    `department_id` = 4 AND `salary` >= 1000
+ORDER BY `id` ASC;
+
+---------------------------------------------------------------
+Problem 6
+DELETE FROM `employees` 
+WHERE
+    `department_id` IN (1 , 2);
+SELECT 
+    *
+FROM
+    `employees`
+ORDER BY `id` ASC;
