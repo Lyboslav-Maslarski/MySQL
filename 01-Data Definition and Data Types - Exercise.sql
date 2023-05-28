c7f6d4ad9e5d0241cb89cf56a14c6ecd859e77bc
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-05-28 05:53:16 +0300

    Update 01-Data Definition and Data Types - Exercise.sql

--- 01-Data Definition and Data Types - Exercise.sql
+++ 01-Data Definition and Data Types - Exercise.sql
@@ -1,12 +1,12 @@
 Problem 1 
 create table `minions`(
-	`id` int primary key auto_increment,
-	`name` varchar(50) not null,
-	`age` int not null
+	id int primary key auto_increment,
+	name varchar(50) not null,
+	age int not null
 );
 create table `towns`(
-	`town_id` int primary key auto_increment,
-	`name` varchar(50) not null
+	town_id int primary key auto_increment,
+	name varchar(50) not null
 );
 
 ---------------------------------------------------------------
